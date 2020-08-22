2f73a3014b198fa1c1a733254f7e0746ea56b6c1
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2020-08-22 17:55:30 +0900

    Graph DFS

--- GraphRepresent.js
+++ GraphRepresent.js
@@ -6,10 +6,15 @@ class GraphByAdjacencyMatrix {
     this.directedGraph = directedGraph;
     this.adjMatrix = [];
 
+    this.verticesFree = new Array(v); // vertice free statuses
+    this.trace = new Array(v); // tracing array for Graph Search
+
     for (let i = 0; i < v; i++) {
       const row = new Array(v);
 
       this.adjMatrix.push(row.fill(0));
+      this.verticesFree[i] = true;
+      this.trace[i] = -1;
     }
   }
 
@@ -28,16 +33,59 @@ class GraphByAdjacencyMatrix {
       console.log();
     }
   }
+
+  DFSHandler(startPoint) {
+    for (let i = 0; i < this.v; i++) {
+      if (this.adjMatrix[startPoint][i] !== 0 && this.verticesFree[i]) {
+        this.verticesFree[i] = false;
+        this.trace[i] = startPoint;
+
+        this.DFSHandler(i);
+      }
+    }
+  }
+
+  printDFSResult(startPoint, endPoint) {
+    if (this.verticesFree[endPoint]) {
+      console.log(`Not found any paths from ${startPoint} to ${endPoint}`);
+    } else {
+      let pivot = endPoint;
+
+      while(true) {
+        process.stdout.write(`${pivot}`);
+        if (pivot !== startPoint) {
+          process.stdout.write(' <- ');
+        } else {
+          console.log();
+          break;
+        }
+        pivot = this.trace[pivot];
+      }
+    }
+  }
+
+  DFS(startPoint, endPoint) {
+    for (let i = 0; i < this.v; i++) {
+      this.verticesFree[i] = true;
+      this.trace[i] = -1;
+    }
+
+    this.trace[startPoint] = startPoint;
+    this.verticesFree[startPoint] = false;
+
+    this.DFSHandler(startPoint);
+    this.printDFSResult(startPoint, endPoint);
+  }
 }
 
-const graph = new GraphByAdjacencyMatrix(5);
+const graph = new GraphByAdjacencyMatrix(8);
 
 graph.addEdge(0, 1);
-graph.addEdge(0, 4);
-graph.addEdge(1, 4);
+graph.addEdge(0, 2);
 graph.addEdge(1, 2);
 graph.addEdge(1, 3);
-graph.addEdge(3, 2);
-graph.addEdge(3, 4);
+graph.addEdge(2, 4);
+graph.addEdge(3, 5);
+graph.addEdge(6, 7);
 
-graph.traverse();
+graph.DFS(3, 4);
