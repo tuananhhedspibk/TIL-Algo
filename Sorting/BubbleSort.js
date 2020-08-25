9546837871511c82ab6f8d14fedc836677ce9284
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2020-08-25 21:02:47 +0900

    Bubble sort

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,5 +1,17 @@
 function bubbleSort(inputs) {
+  let temp = 0;
 
+  for (let i = 1; i < inputs.length; i++) {
+    for (let j = inputs.length - 1; j >= i; j--) {
+      if (inputs[j] < inputs[j - 1]) {
+        temp = inputs[j];
+        inputs[j] = inputs[j - 1];
+        inputs[j - 1] = temp;
+      }
+    }
+  }
+
+  console.log(inputs);
 }
 
-
+bubbleSort([6, 5, 3, 4, 2, 1]);
